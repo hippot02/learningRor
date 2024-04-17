5543909b4775ca313832365e5ecb627cbb9b7400
Hippot <hippot02@gmail.com>
2024-04-17 09:05:59 +0200

    init

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  devise_scope :user do
+    get '/users', to: 'device/registrations#new'
+    get '/users/password', to: 'deviser/passwords#new'
+    get '/users/sign_out' => 'devise/session#destroy'
+  end
+
+  devise_for :users
+  get 'home/about'
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "posts#index"
+end
